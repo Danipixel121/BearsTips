d5a9732dbabcdfcbe1b11ef1cbda4abdb74f5c3c
Daniel <danielduma121@gmail.com>
2026-02-18 16:47:33 +0100

    Elimina adaptador y actualiza workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import staticAdapter from '@astrojs/static';
 
 export default defineConfig({
   site: 'https://Danipixel121.github.io',
   base: '/BearsTips/',
-  adapter: staticAdapter(),
 });
