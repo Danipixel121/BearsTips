c7248cac90045b9ab5aaa9770807b0f955a7ef44
Daniel <danielduma121@gmail.com>
2026-02-18 16:20:49 +0100

    Primer commit - BASE

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import staticAdapter from '@astrojs/static';
 
 export default defineConfig({
-  site: 'https://danipixel121.github.io',
-  base: '/bearstips/',
+  site: 'https://Danipixel121.github.io',
+  base: '/BearsTips/',
   adapter: staticAdapter(),
 });
