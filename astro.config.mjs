9a5f5e936c68156495235076853488709f7ee7d1
Daniel <danielduma121@gmail.com>
2026-02-19 23:58:36 +0100

    descripcion web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: 'https://bearstips.com',
   output: "server",
   adapter: vercel(),
+  integrations: [sitemap()],
 });
