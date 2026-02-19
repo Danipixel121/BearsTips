ce56341aa4784b404b9d3a2d56ab2ae342d1fe38
Daniel <danielduma121@gmail.com>
2026-02-19 21:22:08 +0100

    Primer commit - BASE

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://Danipixel121.github.io',
-  base: '',  // solo si no es tu repo principal (usuario.github.io)
+  output: "server",
 });
